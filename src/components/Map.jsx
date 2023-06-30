fa18efa904962833004734121df859d1177ac70d
ceemoods <carl@ramani.io>
2023-06-30 11:09:12 -0400

    Fix map not centering on country change.

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { MapContainer as Maps, TileLayer } from 'react-leaflet';
+import { MapContainer as Maps, TileLayer, useMap } from 'react-leaflet';
 import '../styles/Map.css';
 import { showDataOnMap } from '../utilities';
+
+const Centerer = ({ center }) => {
+	const map = useMap();
+	useEffect(() => {
+		map.setView(center);
+	}, [center, map]);
+	return null;
+};
 
 function Map({ countries, casesType, center, zoom }) {
 	// The hooks fixes incorrect panning to countries
 	const [center1, setCenter1] = useState(center);
 	useEffect(() => setCenter1(center), [center]);
+
 	return (
 		<div className='map'>
 			<Maps center={center1} zoom={zoom}>
+				<Centerer center={center1} />
 				<TileLayer
 					url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 					attribution='&copy; <a href="http://osm.org/copyright">
